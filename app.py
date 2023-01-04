eb9ebfb83ee355a95abcd5f767d243ef6242fab8
Harper Reed <harper@nata2.org>
2023-01-04 13:28:21 -0600

    updated to use web api instead of mqtt

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import paho.mqtt.client as mqtt
-import os
+from flipdotapi import remote_sign as sign
+import logging
+
+import configparser
+import sys
+from os import path
 app = Flask(__name__)
-
-import paho.mqtt.client as mqtt #import the client1
-
-#broker_address="iot.eclipse.org" #use external broker
-
-
-def send_mqtt_message(message):
-        broker_address=os.environ['BROKER']  # mqtt broker
-        topic = os.environ['TOPIC'] # topic that the flipdot sign is listening on
-        client = mqtt.Client("FlipdotClient") #create new instance
-        client.connect(broker_address) #connect to broker
-        client.publish(topic,message) #publish
 
 @app.route('/')
 def index():
@@ -24,8 +16,30 @@ def index():
 @app.route('/send', methods=['POST'])
 def send():
         message = request.form['message']
-        send_mqtt_message(message)
+        # send_mqtt_message(message)
+        sign.write_text(message, font_name=display_font, fit=False)
         return ("Sent message to sign: " + message)
 
 if __name__ == '__main__':
+        formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
+        logging.basicConfig(level=logging.DEBUG, format=formatter)
+        logger = logging.getLogger(__name__)
+
+        config = configparser.ConfigParser()
+        config_file = 'config.ini'
+        if path.exists(config_file):
+                config.read(config_file)
+        else:
+                logging.error("Config file missing: {}".format(config_file))
+                sys.exit(1)
+
+        sign_url = config.get('FLIPDOT_SERVER', 'URL')
+        sign_columns = config.getint('FLIPDOT_SERVER', 'COLUMNS')
+        sign_rows = config.getint('FLIPDOT_SERVER', 'ROWS')
+        sign_sim = config.getboolean('FLIPDOT_SERVER', 'SIMULATOR')
+
+        display_font = config.get('DISPLAY', 'FONT')
+
+        sign = sign(sign_url, sign_columns, sign_rows, simulator=sign_sim)
+
         app.run(debug=True,host='0.0.0.0')
